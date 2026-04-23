0b548bdcb4a0318ba21208844763a5ac3e6d2416
Kherilirli <januarkherilirli@gmail.com>
2026-04-23 10:08:36 +0700

    Tugas 4-Framework Laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
@@ -11,10 +12,52 @@ class AuthorController extends Controller
     public function index() {
         $authors = Author::all();
 
+        if ($authors->isEmpty()) {
+            return response()->json([
+                "success" => true,
+                "message" => "Resource data not found",
+            ]);
+        }
+
         return response()->json([
             "success" => true,
             "message" => "Get all authors",
             "data" => $authors
         ], 200);
+    }
+
+    public function store(Request $request) {
+        //validator
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string|max:100",
+            "photo" => "required|image|mimes:jpeg,png,jpg|max:2048",
+            "bio" => "required|string"
+        ]);
+
+        //cek validation
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+
+        //upload image
+        $image = $request->file('photo');
+        $image->store('authors', 'public');
+
+        //tambah data
+        $authors = Author::create([
+            "name" => $request->name,
+            "photo" => $image->hashName(),
+            "bio" => $request->bio
+        ]);
+
+        //response
+        return response()->json([
+            "success" => true,
+            "message" => "Author created successfully",
+            "data" => $authors
+        ], 201);
     }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function PHPUnit\Framework\returnArgument;
 
 class GenreController extends Controller
 {
@@ -11,10 +14,46 @@ class GenreController extends Controller
     public function index() {
         $genres = Genre::all();
 
+        if ($genres->isEmpty()) {
+            return response()->json([
+                "success" => true,
+                "message" => "Resource data not found",
+            ]);
+        }
+
         return response()->json([
             "success" => true,
             "message" => "Get all genres",
             "data" => $genres
         ], 200);
+    }
+
+    public function store(Request $request) {
+        //validator
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string|max:100",
+            "description" => "required|string"
+        ]);
+
+        //cek validation
+        if ($validator->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validator->errors()
+            ], 422);
+        }
+
+        //tambah data
+        $genres = Genre::create([
+            "name" => $request->name,
+            "description" => $request->description
+        ]);
+
+        //response
+        return response()->json([
+            "success" => true,
+            "message" => "Genre created successfully",
+            "data" => $genres
+        ], 201);
     }
 }
